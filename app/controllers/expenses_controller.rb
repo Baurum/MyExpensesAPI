e73a09fc82677d16c2d0be3b68c0aea7513c736c
baurumtec <baurumtec@gmail.com>
2017-04-20 13:20:47 -0400

    Add show end point to expensive controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,18 +1,21 @@
 class ExpensesController < ApplicationController
   respond_to :json
   
- #
+ # GET /expenses
   def index
     respond_with Expense.all
   end
   
-  #
+  # GET /expenses/id(id user)
   def show
+    respond_with Expense.find(params[:id])
     
   end
   
-  #
+  # POST /expenses
   def create
+    logger.debug params
+    logger.debug params[:amount]
     
     end
   
